884b49da990fe849304d5d2eab2d68d277908dcf
Zaid.nvr <utegfer@gmail.com>
2026-07-23 12:56:36 -0600

    feat: support packed shader uniforms

--- src/rendering/ShaderProgram.hpp
+++ src/rendering/ShaderProgram.hpp
@@ -27,13 +27,23 @@ public:
     bool setInt(char const* uniformName, int value) const;
     bool setFloat(char const* uniformName, float value) const;
     bool setVec2(char const* uniformName, float x, float y) const;
+    bool setVec4(
+        char const* uniformName,
+        float x,
+        float y,
+        float z,
+        float w
+    ) const;
     void reset();
 
     [[nodiscard]] bool isLoaded() const;
     [[nodiscard]] bool isValid() const;
 
 private:
-    bool loadFromSource(std::string const& vertexSource, std::string const& fragmentSource);
+    bool loadFromSource(
+        std::string const& vertexSource,
+        std::string const& fragmentSource
+    );
     static std::string readTextFile(std::filesystem::path const& path);
     int uniformLocation(char const* uniformName) const;
 
